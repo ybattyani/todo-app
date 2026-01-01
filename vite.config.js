1faba2e8e1695db350b234152e8d24bf80d5e836
Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>
2026-01-01 15:33:38 -0500

    fix vite.config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/todo-app/",
   plugins: [
     react(),
     VitePWA({
@@ -10,24 +11,23 @@ export default defineConfig({
       manifest: {
         name: "To-Do App",
         short_name: "To-Do",
-        start_url: "/",
+        start_url: "/todo-app/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#000000",
         icons: [
           {
-            src: "icon-192.png",
+            src: "/todo-app/icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "icon-512.png",
+            src: "/todo-app/icon-512.png",
             sizes: "512x512",
             type: "image/png"
           }
         ]
       }
     })
-  ],
-  base: "/todo-app/",
+  ]
 });
