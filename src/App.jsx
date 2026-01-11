f40c5cdfea1a91cd477623ff59e86d11d4af6905
Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>
2026-01-11 10:29:47 -0500

    sorting with parent
    Signed-off-by: Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 import TaskCreateModal from "./models/TaskCreationModal";
-import { TASK_CATEGORIES,buildTaskTree } from "./utils/Taskutils";
 import TaskModal from "./models/Task";
+import { buildTaskTree,sortTasks } from "./utils/Taskutils";
 import { subscribeToTasks,addTaskToDB } from "./utils/db";
 import './App.css'
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [sortBy, setSortBy] = useState('completed'); // 'dueDate', 'category', etc.
-  const [sortOrder, setSortOrder] = useState("asc"); // 'asc' or 'desc'
-  const taskTree = buildTaskTree(tasks);
+
+  const taskTree = buildTaskTree(sortTasks(tasks));
 
   useEffect(() => {
     const unsubscribe = subscribeToTasks(setTasks);
@@ -24,19 +23,6 @@ function App() {
   const openCreateModal = () => {
     setIsModalOpen(true);
   };
-  const sortedTasks = () => {
-    let sorted = [...tasks];
-    if (sortBy) {
-      sorted.sort((a, b) => {
-        if (sortOrder === "asc") {
-          return a[sortBy] > b[sortBy] ? 1 : -1;
-        } else {
-          return a[sortBy] < b[sortBy] ? 1 : -1;
-        }
-      });
-    }
-    return sorted;
-  };
 
   return (
     <div style={{ padding: 20 }}>
@@ -44,24 +30,6 @@ function App() {
       <button onClick={() => openCreateModal()} className="task-add-btn">
         Add Task
       </button>
-      <div className="sort-controls">
-        <label>Sort by: </label>
-        <select
-          value={sortBy || ""}
-          onChange={(e) => setSortBy(e.target.value)}
-        >
-          <option value="completed">Completed</option>
-          <option value="dueDate">Due Date</option>
-          <option value="category">Category</option>
-          <option value="text">Text</option>
-        </select>
-
-        <button onClick={() =>
-          setSortOrder((prev) => (prev === "asc" ? "desc" : "asc"))
-        }>
-          {sortOrder === "asc" ? "↑" : "↓"}
-        </button>
-      </div>
 
       {isModalOpen && <TaskCreateModal
         task={null}
