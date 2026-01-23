8126f5c267d9780d5d5b21c57603b2820bfadbb0
Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>
2026-01-23 08:05:48 -0500

    fix main page button
    Signed-off-by: Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -8,15 +8,15 @@ export default function MainPage() {
       <h1>Main Page</h1>
       <p>Welcome!! <br/>In this app you will be able to manage your tasks and routines</p>
       <div className="big-actions">
-        <NavLink to="/todo-app" className="action-tile">
+        <NavLink to="/todo-app/todoList" className="action-tile">
           <ListTodo size={60} />
           <span>Tasks</span>
         </NavLink>
-        <NavLink to="/morning-flow" className="action-tile">
+        <NavLink to="/todo-app/morning-flow" className="action-tile">
           <Sun size={60} />
           <span>Morning Flow</span>
         </NavLink>
-        <NavLink to="/backlog" className="action-tile">
+        <NavLink to="/todo-app/backlog" className="action-tile">
           <NotepadText size={60} />
           <span>Backlog</span>
         </NavLink>
