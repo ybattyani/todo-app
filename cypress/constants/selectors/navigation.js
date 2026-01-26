5fc678662c1d342cf773dde7f655d49004236100
Yoann <yoann@battyani.com>
2026-01-26 15:59:29 -0500

    Update navigation.js

--- cypress/constants/selectors/navigation.js
+++ cypress/constants/selectors/navigation.js
@@ -3,6 +3,7 @@ export const routes = [
   { selector: "nav-morning", path: "/todo-app/morning-flow", title: "Morning Flow"},
   { selector: "nav-backlog", path: "/todo-app/backlog", title: "Backlog"},
   { selector: "nav-today", path: "/todo-app/today", title: "Today"},
+  { selector: "nav-grocery", path: "/todo-app/grocery", title: "Grocery"},
 ];
 
 export const navSelectors = {
