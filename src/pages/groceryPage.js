377058fcfea8a1da92162789d10d09314d1b831d
Yoann <yoann@battyani.com>
2026-01-26 15:40:04 -0500

    Add GroceryPage component for grocery list display

---
+++ src/pages/groceryPage.js
@@ -0,0 +1,9 @@
+
+export default function GroceryPage{
+  return (
+    <div className="page grocery-list">
+      <h1>Grocery List</h1>
+      <p>your grocery list for today</p>
+    </div>
+  );
+}
