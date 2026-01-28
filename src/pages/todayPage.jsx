34267306d77deb15206c9a41542b1974533bba42
Yoann <yoann@battyani.com>
2026-01-28 16:23:57 -0500

    Update todayPage.jsx

--- src/pages/todayPage.jsx
+++ src/pages/todayPage.jsx
@@ -2,16 +2,18 @@ import { useState, useEffect } from "react";
 import { AddTaskButton } from "../models/TaskList";
 import TaskCreateModal from "../models/TaskCreationModal";
 import TaskModal from "../models/Task";
-import { todayAndOverdueTasks,sortTasks } from "../utils/Taskutils";
+import { todayAndOverdueTasks,sortTasks, onlyTomorrowTasks } from "../utils/Taskutils";
 import { subscribeToTasks,addTaskToDB } from "../utils/db";
 
 export default function todayPage() {
   const [tasks, setTasks] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isToday, setIsToday] = useState(true)
   const filteredTasks = (sortTasks(todayAndOverdueTasks(tasks)));
 
   useEffect(() => {
     const unsubscribe = subscribeToTasks(setTasks);
+    setTasks((sortTasks(todayAndOverdueTasks(tasks))))
     return () => unsubscribe();
   }, []);
   async function handleSaveTask(task) {
@@ -19,11 +21,20 @@ export default function todayPage() {
     setTasks((prev) => [...prev, task]);
     await addTaskToDB(task);
   };
+  const handleDayChange = () => {
+    setIsToday(!isToday)
+    if(isToday){
+       setTasks((sortTasks(todayAndOverdueTasks(tasks))))
+}else{
+  setTasks((sortTasks(onlyTomorrowTasks(tasks))))
+}
+}
 
   return (
     <div className="page morning-flow">
       <h1>Today</h1>
       <p>Here is today's list of task</p>
+      <button onClick{handleDayChange}>change day</button>
       {AddTaskButton(()=>setIsModalOpen(true))}
       {isModalOpen && <TaskCreateModal
         task={null}
@@ -31,9 +42,9 @@ export default function todayPage() {
         onClose={() => setIsModalOpen(false)}
         onSave={handleSaveTask}
       />}
-      {filteredTasks.length === 0 && <div>Congrats nothing to do today</div>}
+      {tasks.length === 0 && <div>Congrats nothing to do today</div>}
       <ul>
-        {filteredTasks.map(task => (
+        {tasks.map(task => (
           <TaskModal key={task.id} task={task} />
         ))}
       </ul>
