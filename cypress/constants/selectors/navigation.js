77dffd0a7ed41bb5fe75af7b6176fa3a03e1f320
Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>
2026-01-28 14:51:35 -0500

    fix navigation test
    Signed-off-by: Yoann Battyani <yoannbattyani@Yoanns-MacBook-Pro-2.local>

--- cypress/constants/selectors/navigation.js
+++ cypress/constants/selectors/navigation.js
@@ -1,8 +1,8 @@
 export const routes = [
   { selector: "nav-tasks", path: "/todo-app/todoList", title: "To Do List"},
+  { selector: "nav-today", path: "/todo-app/today", title: "Today"},
   { selector: "nav-morning", path: "/todo-app/morning-flow", title: "Morning Flow"},
   { selector: "nav-backlog", path: "/todo-app/backlog", title: "Backlog"},
-  { selector: "nav-today", path: "/todo-app/today", title: "Today"},
   { selector: "nav-grocery", path: "/todo-app/grocery", title: "Grocery List"},
 ];
 
